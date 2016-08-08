930f8fdfbbe1a68ffa3d6c1d2c9d503590097eed
Matthijs Overboom <matthijsske@gmail.com>
2016-08-08 16:54:01 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+task
+====
+
+A Symfony project created on August 6, 2016, 9:24 pm.
---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * @Rout
+     */
+    public function IndexAction() {
+
+    }
+}
